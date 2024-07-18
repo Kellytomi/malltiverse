0554a83e0a1615bf93e2d1cce6787a91e128a1aa
Etoma-etoto Odi <kelvinetoma95@gmail.com>
2024-07-18 03:00:43 +0100

    fixed animation

--- lib/payment_success_page.dart
+++ lib/payment_success_page.dart
@@ -8,7 +8,7 @@ class PaymentSuccessPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Navigate back to home after 3 seconds
-    Timer(Duration(seconds: 3), () {
+    Timer(const Duration(seconds: 4), () {
       Navigator.pushReplacementNamed(context, '/home');
     });
 
@@ -36,14 +36,11 @@ class PaymentSuccessPage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: Lottie.asset(
-                      'assets/animations/tick.json',
-                      fit: BoxFit.contain,
-                    ),
+                  width: 150,
+                  height: 150,
+                  child: Lottie.network(
+                    'https://lottie.host/a2a58f81-4c5d-4bf1-80cc-9688731fdf66/sfz3K4lHLQ.json',
+                    fit: BoxFit.contain,
                   ),
                 ),
                 const SizedBox(height: 16),
